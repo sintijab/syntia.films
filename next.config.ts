0ee1e6e681d5a7a9b33a8d975f63403c3e43d057
Sintija Birgele <sintijabirgele@Sintijas-Laptop.local>
2024-10-29 11:35:25 +0100

    feat: assets

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  assetPrefix: process.env.ASSET_PREFIX || "",
+  basePath: '',
+  assetPrefix: '',
   output: 'export',
   images: { unoptimized: true },
 };
